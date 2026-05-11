8cdc02f14b1205b61794c7a712611eab80804e42
kashish <kashishprajapati@kashishs-Air.home.local>
2026-05-10 23:50:26 -0400

    Module 2 Assignment

--- app.js
+++ app.js
@@ -1,10 +1,17 @@
 const express = require('express');
 const path = require('path');
+const hbs = require('hbs');
+
+const routes = require('./app_server/routes/index');
 
 const app = express();
 const port = 3000;
 
+app.set('views', path.join(__dirname, 'app_server', 'views'));
+app.set('view engine', 'hbs');
+
 app.use(express.static(path.join(__dirname)));
+app.use('/', routes);
 
 app.listen(port, () => {
   console.log(`Travlr app listening at http://localhost:${port}`);
